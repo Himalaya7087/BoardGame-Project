14f4ec7efecfdfee8f6e8457bc450755de398368
admin <admin@admins-MacBook-Pro.local>
2024-07-24 17:24:34 +0530

    deployment

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -4,6 +4,6 @@ export ACCOUNT_NO=$1
 export ECR_REPO=$2
 export IMAGE_TAG=$3
 
-envsubst < deployment.yaml.tmpl > deployment.yaml
-envsubst < service.yaml.tmpl > service.yaml
-envsubst < ingress.yaml.tmpl > ingress.yaml
+envsubst < ./deployment.yaml.tmpl > deployment.yaml
+envsubst < ./service.yaml.tmpl > service.yaml
+envsubst < ./ingress.yaml.tmpl > ingress.yaml
